b3ca021f25a9c7b909e63d55f2de2782a4b08c99
Alex Nadreau <anadreau42@gmail.com>
2023-05-04 14:37:20 -0700

    Breaking apart outputStringCreator

--- lib/converter.dart
+++ lib/converter.dart
@@ -11,6 +11,35 @@ import 'package:ffmpeg_converter/utils/common_variables.dart';
 final fileInputStringCreator = Creator.value('', name: 'inputStringCreator');
 final fileNameCreator = Creator.value('', name: 'fileNameCreator');
 
+///String containing full path of file
+final filePathCreator = Creator((ref) {
+  var fileInput = ref.watch(fileInputStringCreator);
+  var parsedFileBySlash = fileInput.split('/');
+  String? filePathResult;
+
+  if (fileInput != '') {
+    List workingParsedFileList = parsedFileBySlash;
+
+    ///String that represents the original filename
+    var oldFileName = parsedFileBySlash.removeLast();
+
+    ///Removes last item in list which should be the old file name
+    workingParsedFileList.removeRange(
+        parsedFileBySlash.length, parsedFileBySlash.length);
+
+    ///Joins the workingParsedFileList by / into single String
+    String joinedOutput = workingParsedFileList.join('/');
+
+    ///finds the index where the file type starts based on last '.'
+    int filetypeIndex = oldFileName.lastIndexOf('/');
+
+    filePathResult = joinedOutput.substring(0, filetypeIndex);
+  } else {
+    filePathResult = null;
+  }
+  return filePathResult;
+});
+
 ///String containing full file path and new name used for converted file.
 ///
 ///Takes inputStringCreator value and parses and converts it into new file name.
