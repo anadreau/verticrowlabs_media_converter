d17f5f45ffddbccabe08db29e1395342e088fd63
Alex Nadreau <anadreau42@gmail.com>
2024-03-11 14:37:32 -0700

    add thumbnail for selected file. Fixes #25

--- lib/thumbnail_grabber/thumbnail_generator.dart
+++ lib/thumbnail_grabber/thumbnail_generator.dart
@@ -1,9 +1,9 @@
-import 'package:ffmpeg_converter/file_parsing/file_parsing_barrel.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:ffmpeg_converter/file_parsing/file_parsing_barrel.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
 /*
 ffmpeg -i .\hhd800.com@BMW-301.mp4 -vf "select='eq(pict_type,PICT_TYPE_I)'" -vsync vfr -ss 00:00:30 -vframes 1 thumbnail2.jpg
 */
 
-final thumbnailGeneratorProvider = Provider((ref) {
-  final fileInput = ref.watch(fileInputStringProvider);
-});
+// final thumbnailGeneratorProvider = Provider((ref) {
+//   final fileInput = ref.watch(fileInputStringProvider);
+// });
