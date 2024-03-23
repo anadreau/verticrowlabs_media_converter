ce645137533cab2f75f562c7d4037adf970ea0eb
Alex Nadreau <anadreau42@gmail.com>
2024-03-22 22:35:57 -0700

    Named start and stop time providers.

--- lib/media_snipping/media_snipping.dart
+++ lib/media_snipping/media_snipping.dart
@@ -1,13 +1,17 @@
 //TO-DO: #34 Implement video snipping. @anadreau
 
 //example: ffmpeg -i input.mp4 -ss 00:05:00 -to 00:06:00 output.mp4
-//example: ffmpeg -i input.mp4 -ss 00:00:00 -to 00:00:00 -vf scale=1920:1080 
+//example: ffmpeg -i input.mp4 -ss 00:00:00 -to 00:00:00 -vf scale=1920:1080
 // -c:v libx264 output.mp4
 
 //ffmpeg cmd
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+String ffmpegCmd = '';
 //variable [Provider]<String> to hold -ss start time in 00:00:00 format
 //variable defaults to '' when not used
-
+///
+final startTimeProvider = Provider((ref) => '');
 //variable [Provider]<String> to hold -to stop time in 00:00:00 format
 //variable defaults to '' when not used
+final endTimeProvider = Provider((ref) => '');
