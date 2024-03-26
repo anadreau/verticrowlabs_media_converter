fe6b5746d5b2a426797b71976493eeb679c591c7
Alex Nadreau <anadreau42@gmail.com>
2024-03-26 14:18:49 -0700

    researching ffprobe to get media duration.

--- lib/media_snipping/media_snipping.dart
+++ lib/media_snipping/media_snipping.dart
@@ -7,3 +7,7 @@ final startTimeProvider = Provider((ref) => '');
 ///variable [Provider]<String> to hold -to stop time in 00:00:00 format
 ///variable defaults to '' when not used
 final endTimeProvider = Provider((ref) => '');
+
+
+// ffprobe cmd to get duration:
+//ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1 -sexagesimal
