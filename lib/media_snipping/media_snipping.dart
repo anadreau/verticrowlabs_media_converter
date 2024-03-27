4af664392110a445e7e4b75a0c2199aa71748453
Alex Nadreau <anadreau42@gmail.com>
2024-03-27 15:09:01 -0700

    set up MediaTime class with Duration and mediaTimeFromString functions.

--- lib/media_snipping/media_snipping.dart
+++ lib/media_snipping/media_snipping.dart
@@ -6,6 +6,48 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:verticrowlabs_media_converter/file_parsing/file_input.dart';
 import 'package:verticrowlabs_media_converter/utils/utils_barrel.dart';
 
+///[MediaTime] class to handle time information for selected media
+///such as start time, end time, and duration.
+class MediaTime {
+  ///default consructor of [MediaTime]
+  MediaTime({
+    this.hours,
+    this.minutes,
+    this.seconds,
+  });
+
+  ///Media time hrs:Mins:seconds
+  int? hours;
+
+  ///Media time hrs:Mins:seconds
+  int? minutes;
+
+  ///Media time hrs:Mins:seconds
+  double? seconds;
+
+  ///function to parse from string to [MediaTime]
+  MediaTime mediaTimeFromString(String durationString) {
+    final parsedDuration = durationString.substring(9);
+    final tempList = parsedDuration.split(':');
+    final tempHours = int.parse(tempList[0]);
+    final tempMinutes = int.parse(tempList[1]);
+    final tempSeconds = double.parse(tempList[2]);
+    return MediaTime(
+      hours: tempHours,
+      minutes: tempMinutes,
+      seconds: tempSeconds,
+    );
+  }
+
+  ///function parse from int to String in format 00:00:00.000
+  String duration(int? hours, int? minutes, double? seconds) {
+    final durationhrs = hours.toString();
+    final durationmins = minutes.toString();
+    final durationseconds = seconds.toString();
+    return '$durationhrs:$durationmins:$durationseconds';
+  }
+}
+
 ///variable [StateProvider]<String> to hold -ss start time in 00:00:00 format
 ///variable defaults to '' when not used
 final startTimeProvider = StateProvider((ref) => '');
@@ -41,8 +83,9 @@ ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1 -s
   if (result.exitCode == 0) {
     log('Probe stdout: ${result.stdout}');
     log('Probe stderr: ${result.stderr}');
-    final duration = result.stdout.toString();
-    final durationFormatted = duration.substring(9);
+    final duration = MediaTime().mediaTimeFromString(result.stdout.toString());
+    final durationFormatted =
+        duration.duration(duration.hours, duration.minutes, duration.seconds);
     log('Duration: $durationFormatted');
     ref.read(maxTimeProvider.notifier).update((state) => durationFormatted);
   } else {
@@ -50,8 +93,7 @@ ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1 -s
   }
 }
 
-
 // Create function that parses the duration by splitting it by ":"
 // then changing to int and validating it against the input numbers
-// if all three values are equal or lower than max then allow the 
-// input -ss & -to time to be used. 
+// if all three values are equal or lower than max then allow the
+// input -ss & -to time to be used.
