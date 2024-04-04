4cf7003f61e98e9e4c93998da00f25d202eb23d9
Alex Nadreau <anadreau42@gmail.com>
2024-04-03 23:32:37 -0700

    FfmpegInstaller all passed tests.

--- test/ffmpeg_installer_test.dart
+++ test/ffmpeg_installer_test.dart
@@ -3,8 +3,34 @@ import 'package:verticrowlabs_media_converter/features/install_ffmpeg/ffmpeg_ins
 
 void main() {
   group('Testing FfmpegInstaller:', () {
-    test('Creates dir', () {
-      FfmpegInstaller().createDir();
+    test('Creates dir', () async {
+      expect(await FfmpegInstaller().createDir(), true);
+    });
+
+    test(
+      'Download Ffmpeg',
+      () async {
+        expect(await FfmpegInstaller().downloadFfmpeg(), true);
+      },
+      timeout: const Timeout(Duration(minutes: 5)),
+    );
+    test('Extract FFmpeg', () async {
+      expect(await FfmpegInstaller().extractFfmpeg(), true);
+    });
+    test('Move Ffmpeg', () async {
+      expect(await FfmpegInstaller().moveFfmpeg(), true);
+    });
+    test('Clean up dir', () async {
+      expect(await FfmpegInstaller().cleanFfmpegDir(), true);
+    });
+    test('Set Path Variable', () async {
+      expect(await FfmpegInstaller().setPathVariable(), true);
+    });
+    test('Update Path Variable', () async {
+      expect(await FfmpegInstaller().updatePathVariable(), true);
+    });
+    test('Update Path Variable', () async {
+      expect(await FfmpegInstaller().updatePathVariable(), true);
     });
   });
 }
